eaa2201c71e1dc495eb32c504051a453e805e092
jasekjan <jasekjaan@gmail.com>
2018-09-25 00:20:00 +0200

    Init commit

--- app/src/main/java/com/development/honza/jasichinese/db/Settings.java
+++ app/src/main/java/com/development/honza/jasichinese/db/Settings.java
@@ -4,18 +4,25 @@ import java.util.Set;
 
 public class Settings {
     private float speed;
+    private String path;
 
     public Settings() {};
 
-    public Settings(float speed) {
+    public Settings(float speed, String path) {
         this.speed = speed;
+        this.path = path;
     }
 
     public float getSpeed() {
         return speed;
     }
 
+    public String getPath() {
+        return path;
+    }
     public void setSpeed(float speed) {
         this.speed = speed;
     }
+
+    public void setPath(String path) { this.path = path;}
 }
